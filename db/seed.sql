d851348d1ba5d1836baa6aca32510d946ae4e996
CloudyLondon <jovialcty@gmail.com>
2022-03-08 14:00:39 -0800

    npm

--- db/seed.sql
+++ db/seed.sql
@@ -4,4 +4,4 @@ INSERT INTO Product (id, product_name, price, stock, category_id);
 
 INSERT INTO Tag (id, tag_name);
 
-INSERT INTO ProductTag (id, product_id, tag_id)
+INSERT INTO ProductTag (id, product_id, tag_id);
