75fbc00cd27662787b410ae83cb9640ff3cc40ab
CloudyLondon <jovialcty@gmail.com>
2022-03-09 11:03:27 -0800

    fix bug

--- db/seed.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO Category (id, category_name);
-
-INSERT INTO Product (id, product_name, price, stock, category_id);
-
-INSERT INTO Tag (id, tag_name);
-
-INSERT INTO ProductTag (id, product_id, tag_id);
